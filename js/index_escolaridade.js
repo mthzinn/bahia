155d6928978399d2404d26c2ec9065d22f5b2d03
Brunno <brunnopereira090@gmail.com>
2023-08-30 18:30:02 -0300

    faltam alguns detalhes

--- js/index_escolaridade.js
+++ js/index_escolaridade.js
@@ -1,19 +1,16 @@
-let ctx = document.getElementById('grafico_linha')
+let ctx = document.getElementById('grafico_linha');
+Chart.defaults.borderColor = 'white';
+Chart.defaults.color = 'white';
+Chart.defaults.font.size = 21;
+Chart.defaults.font.family = 'Segoe UI';
+Chart.defaults.font.weight = 'bold';
 
-//Configuração Gráfico
-//Grafico 1
-Chart.defaults.borderColor = 'white'
-Chart.defaults.color = 'white'
-Chart.defaults.font.size = 21
-Chart.defaults.font.family = 'Segoe UI'
-Chart.defaults.font.weight = 'bold'
-
-const labels = ['5° Ano Incompleto','5° Ano Completo', '6° ao 9° Ano Incompleto', 'Ensino Médio Incompelto','Ensino Médio Completo','Fundamental Completo','Superior Incompleto','Superior Completo']
+const labels = ['5° Ano Incompleto','5° Ano Completo', '6° ao 9° Ano Incompleto', 'Ensino Médio Incompleto','Ensino Médio Completo','Fundamental Completo','Superior completo', 'Analfabeto'];
 
 const data = {
     labels,
     datasets: [{
-        data: [207, 90, 196, 96, 110, 103,4,1 ],
+        data: [138, 66, 137, 58, 80, 69, 69, 76],
         label: 'Escolaridade dos resgatados residentes na UF',
         backgroundColor: 'white',
         borderColor: 'white',
@@ -24,12 +21,40 @@ const data = {
 const chart = new Chart(ctx, {
     type: 'line',
     data: data,
-    options: {
-      // Aqui você pode adicionar opções adicionais de configuração do gráfico, se necessário
+    options: {}
+});
+
+
+
+let ctx2 = document.getElementById('grafico_linha2');
+// Note que aqui eu criei um novo objeto Chart.defaults para o segundo gráfico
+let chartDefaults2 = {
+    borderColor: 'white',
+    color: 'white',
+    font: {
+        size: 21,
+        family: 'Segoe UI',
+        weight: 'bold'
     }
-  });
+};
 
+// Chart.defaults2 = chartDefaults2;
 
+// const labels2 = ['5° Ano Incompleto','5° Ano Completo', '6° ao 9° Ano Incompleto', 'Ensino Médio Incompleto','Ensino Médio Completo','Fundamental Completo','Superior completo', 'Analfabeto'];
 
+// const data2 = {
+//     labels: labels2,
+//     datasets: [{
+//         data: [138, 66, 137, 58, 80, 69, 69, 76],
+//         label: 'Escolaridade dos resgatados residentes na UF',
+//         backgroundColor: 'white',
+//         borderColor: 'white',
+//         borderWidth: 1
+//     }],
+// }
 
- 
+// const chart2 = new Chart(ctx2, {
+//     type: 'line',
+//     data: data2,
+//     options: {}
+// });
